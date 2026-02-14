de1f4c9409a4ffe17be4aff5e2317e2d3ee4a17b
Nachiiet More <nachiketmore.more@gmail.com>
2026-02-14 14:07:36 +0530

    added mock data

---
+++ frontend/src/lib/mock-data.ts
@@ -0,0 +1,43 @@
+export const mockTransactions = [
+		{
+			title: "Sent to US Nachiket",
+			date: "2026-02-12T10:01:25.442Z",
+			amount: 110,
+			direction: "OUT",
+			referenceId: "fadd55e2-3114-421c-a1c1-43e9654ee343"
+		},
+		{
+			title: "Added Money",
+			date: "2026-02-12T10:00:34.340Z",
+			amount: 100,
+			direction: "IN",
+			referenceId: "79bc393f-8ca4-4b7d-bfc3-cd09d55165e5"
+		},
+		{
+			title: "Added Money",
+			date: "2026-02-02T12:00:00.000Z",
+			amount: 600,
+			direction: "IN",
+			referenceId: "u1-pay-002"
+		},
+		{
+			title: "Added Money",
+			date: "2026-02-01T10:00:00.000Z",
+			amount: 400,
+			direction: "IN",
+			referenceId: "u1-pay-001"
+		}
+	]
+
+export const mockBalance = {
+	message: "Balance fetched",
+	source: "db",
+	balance: 990
+}
+
+export const mockUserDetails = {
+	firstName: "IND",
+	lastName: "Nachiket",
+	email: "ind.nachiket@gmail.com",
+	role: "USER"
+} 
