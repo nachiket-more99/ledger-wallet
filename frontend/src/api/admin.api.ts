0409d062dc1ee642c4a9b2d26252f3a14968f6cb
Nachiiet More <nachiketmore.more@gmail.com>
2026-02-17 03:18:54 +0530

    updated AdminUsers

--- frontend/src/api/admin.api.ts
+++ frontend/src/api/admin.api.ts
@@ -2,7 +2,7 @@ import { http } from "./http";
 
 export const getUsers = async () => {
   const res = await http.get("/admin/users");
-  return res.data.users; 
+  return res.data; 
 };
 
 // export const getTransactions = async () => {
--- frontend/src/hooks/useAdminUsers.ts
+++ frontend/src/hooks/useAdminUsers.ts
@@ -1,11 +1,10 @@
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { getUsers } from "@/api/admin.api";
 
 export const useAdminUsers = () => {
   return useQuery({
     queryKey: ["admin", "users"], 
     queryFn: getUsers,
-    placeholderData: keepPreviousData, 
     staleTime: 10_000,
   });
 };
